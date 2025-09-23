893c8f5661612c47260e9ebb4769a5f65a0c6051
Neville Tummon <nt.devs@gmail.com>
2025-09-23 20:00:46 +1000

    add anthropic API integration and update dependencies

--- experiment_1/main.py
+++ experiment_1/main.py
@@ -1,11 +1,25 @@
-from dotenv import load_dotenv
 from os import getenv
+
+from anthropic import Anthropic
+from dotenv import load_dotenv
 
 _ = load_dotenv()
 
 
 def main():
-    print(getenv("API_KEY"))
+    client = Anthropic(api_key=getenv("ANTHROPIC_API_KEY"))
+
+    message = client.messages.create(
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": "Hello, Claude.",
+            }
+        ],
+        model="claude-3-5-haiku-20241022",
+    )
+    print(message.content)
 
 
 if __name__ == "__main__":

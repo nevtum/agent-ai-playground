d7654db72fa31b7a60ae67fe94b9178923c84e24
Neville Tummon <nt.devs@gmail.com>
2025-10-04 17:54:45 +1000

    add tool calling and permission prompt to agent and new tools module

--- openai_scratch/experiment_2/agent.py
+++ openai_scratch/experiment_2/agent.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from os import getenv
 from openai import OpenAI
 from pathlib import Path
+from .tools import call_tool, TOOLS
 
 _ = load_dotenv()
 
@@ -10,24 +11,45 @@ MODEL: str = getenv("OPENAI_MODEL") or "gpt-4o-mini"
 openai_client = OpenAI(api_key=getenv("OPENAI_API_KEY"))
 
 
+def make_system_prompt(prompt: str) -> dict[str, str]:
+    return {"role": "system", "content": prompt}
+
+
+def make_user_prompt(prompt: str) -> dict[str, str]:
+    return {"role": "user", "content": prompt}
+
+
 class Agent:
     system_prompt: str = (Path(__file__).parent / "prompt.txt").read_text()
 
     def __init__(self, model: str = MODEL):
         self.model: str = model
+        self.messages = [make_system_prompt(self.system_prompt)]
 
     def send_message(self, text: str):
+        self.messages.append(make_user_prompt(text))
+
         response = openai_client.chat.completions.create(
             model=self.model,
+            # tools=TOOLS,
             temperature=0.3,
-            messages=[
-                {"role": "system", "content": self.system_prompt},
-                {
-                    "role": "user",
-                    "content": text,
-                },
-            ],
+            messages=self.messages,
         )
 
         result = response.choices[0].message.content
         return result
+
+        # for item in response.output:
+        #     if item.type == "function_call":
+        #         if item.name == "get_horoscope":
+        #             # 3. Execute the function logic for get_horoscope
+        #             horoscope = get_horoscope(json.loads(item.arguments))
+
+        #             # 4. Provide function call results to the model
+        #             input_list.append({
+        #                 "type": "function_call_output",
+        #                 "call_id": item.call_id,
+        #                 "output": json.dumps({
+        #                   "horoscope": horoscope
+        #                 })
+        #             })
---
+++ openai_scratch/experiment_2/tools.py
@@ -0,0 +1,41 @@
+import subprocess
+from textwrap import dedent
+
+from .permissions import human_gate
+
+
+@human_gate
+def call_system_command(command: str) -> str:
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        if result.returncode != 0:
+            return dedent(f"Error: {result.stderr.strip()}")
+        return result.stdout.strip()
+    except Exception as ex:
+        return f"Failed to call system command: {ex}"
+
+
+def call_tool(name, **kwargs) -> str:
+    if name == call_system_command.__name__:
+        return call_system_command(**kwargs)
+    else:
+        return f"Unknown tool: {name}"
+
+
+TOOLS = {
+    "type": "function",
+    "name": "call_system_command",
+    "description": "Executes a system command in the shell and returns the output or error message.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "command": {
+                "type": "string",
+                "description": "The system command to execute.",
+            }
+        },
+        "required": ["command"],
+        "additionalProperties": False,
+    },
+    "strict": True,
+}

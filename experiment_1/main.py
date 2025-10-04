0ebdd4d73c9718aef0453f4b7b42b9da9429ff33
Neville Tummon <nt.devs@gmail.com>
2025-10-04 11:36:24 +1000

    update prompts

--- experiment_1/main.py
+++ experiment_1/main.py
@@ -1,28 +1,37 @@
 from pprint import pprint
-from textwrap import dedent
 
 from anthropic import Anthropic
 from anthropic.types import MessageParam, ToolUseBlock
 
 from .config import cfg
-from .tools import handle_tool_call, TOOLS
+from .tools import TOOLS, handle_tool_call
 
 
 def output_text(text: str):
     print(text)
 
 
+system_prompt = """
+You are an AI assistant that helps people calculate numbers. \
+Describe the plan how you will answer the question then \
+execute on that plan. Provide only the answer requested and \
+nothing else.
+"""
+
+user_prompt = """
+Calculate a magic number using a series of random numbers. \
+In your final response, output only the calculation in json \
+results similar to the following example:
+
+{
+    "result": 0.23626
+}
+"""
+
+
 def main():
     client = Anthropic(api_key=cfg.api_key)
 
-    system_prompt = dedent("""
-        You are an AI assistant that helps people calculate numbers. \
-        Describe the plan how you will answer the question then \
-        execute on that plan. Provide only the answer requested and \
-        nothing else.
-        """)
-
-    user_prompt = "Can you help me calculate a magic number using a series of random numbers? Output the final calculation in <result></result>"
     output_text(user_prompt)
 
     messages: list[MessageParam] = [
